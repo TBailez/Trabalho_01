7f8c8efd4ac346b7fc9b95ea8f15026be8d18fdb
TBailez <52189272+TBailez@users.noreply.github.com>
2021-05-17 19:36:23 -0300

    Add files via upload

---
+++ Trabalho_01/test.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_excel('tabuas.xlsx')
+
+dfc = df.columns
+at = pd.DataFrame(df, columns = ['AT-2000 FEMALE','AT-2000 MALE'])
+breme15 = pd.DataFrame(df, columns = ['BR-EMSmt-v.2015-f','BR-EMSmt-v.2015-m'])
+at.dropna(inplace=True) 
+breme15.dropna(inplace=True)
