50f6148e52d2287c4f1411aa298dd1ce29bf9f1e
Tomas <tomasbailez11@gmail.com>
2021-05-18 19:51:50 -0300

    adicionei as idades nos dfs

--- Trabalho_01/test.py
+++ Trabalho_01/test.py
@@ -3,7 +3,7 @@ import pandas as pd
 df = pd.read_excel('tabuas.xlsx')
 
 dfc = df.columns
-at = pd.DataFrame(df, columns = ['AT-2000 FEMALE','AT-2000 MALE'])
-breme15 = pd.DataFrame(df, columns = ['BR-EMSmt-v.2015-f','BR-EMSmt-v.2015-m'])
+at = pd.DataFrame(df, columns = ['TÁBUA','AT-2000 FEMALE','AT-2000 MALE'])
+breme15 = pd.DataFrame(df, columns = ['TÁBUA','BR-EMSmt-v.2015-f','BR-EMSmt-v.2015-m'])
 at.dropna(inplace=True) 
 breme15.dropna(inplace=True)
