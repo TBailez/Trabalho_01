03e6a079b4e0fb8a1afce80885a346eeae6affbe
Tomas <tomasbailez11@gmail.com>
2021-05-20 19:13:06 -0300

    montar as tabelas de at 2000 e br ems completas

--- Trabalho_01/test.py
+++ Trabalho_01/test.py
@@ -8,29 +8,56 @@ dfc = df.columns
 #contador para funções while
 c = 0
 #%%
-#Selecionar o gênero
-while True:
-    g = input("F OU M?\n")
+#montando o df de at-2000
+t = input('AT-2000 ou BR-EMSmt-v.2015? Selecione com A ou B\n')
+if t.upper() == 'A':
+    at = pd.DataFrame(df, columns = ['TÁBUA','AT-2000 FEMALE','AT-2000 MALE'])
+    at.columns = ['IDADE', 'AT-2000-F', 'AT-2000-M']
+    at.dropna(inplace=True)
+    at = at.iloc[1:]
+    tam = len(at['IDADE'])
+    while True:
+        g = input("Selecione o gênero com F ou M?\n")
  
-    if g.upper() == 'F':
-        s = 'AT-2000-F'
-        break
+        if g.upper() == 'F':
+            s = 'AT-2000-F'
+            break
 
-    elif g.upper()== 'M':
-        s = 'AT-2000-M'
-        break
-    else:
-        print('Tente selecionar m ou f')
-# %%  
+        elif g.upper()== 'M':
+            s = 'AT-2000-M'
+            break
+        else:
+            print('Tente selecionar F ou M')
+            
+elif t.upper() == 'B':  
+    at = pd.DataFrame(df, columns = ['TÁBUA','BR-EMSmt-v.2015-f','BR-EMSmt-v.2015-m'])
+    at.columns = ['IDADE', 'BR-EMSmt-v.2015-F', 'BR-EMSmt-v.2015-M']
+    at.dropna(inplace=True)
+    at = at.iloc[1:]
+    at = at[:-1]
+    tam = len(at['IDADE'])
+    while True:
+        g = input("Selecione o gênero com F ou M?\n")
+ 
+        if g.upper() == 'F':
+            s = 'BR-EMSmt-v.2015-F'
+            break
+
+        elif g.upper()== 'M':
+            s = 'BR-EMSmt-v.2015-M'
+            break
+        else:
+            print('Tente selecionar F ou M') 
+# %%
 #Selecionar a taxa de juros
 while True:      
-    i = input('Qual a taxa de juros?\n')
+    i = input('Qual a taxa de juros? Responda com 0.01 para 1% ao ano\n')
     try:
         j = float(i)
         break
     except ValueError:
         print('Tente colocar um número')
-# %%
+#%%
 #Selecionar a população inicial
 while True:      
     l = input('Qual a população inicial?\n')
@@ -43,11 +70,6 @@ while True:
 #lista de colunas para adicionar ao dataframe
 col_list = ['lx', 'dx','v^x','Dx','Nx','Cx','Mx']
 
-#montando o df de at-2000
-at = pd.DataFrame(df, columns = ['TÁBUA','AT-2000 FEMALE','AT-2000 MALE'])
-at.columns = ['IDADE', 'AT-2000-F', 'AT-2000-M']
-at.dropna(inplace=True)
-at = at.iloc[1:]
 #%%
 #adiocionando todas as colunas ao dataframe
 for i in col_list:
@@ -55,27 +77,31 @@ for i in col_list:
     
 #atribuindo o valor de l0 para a primeira coluna de lx 
 at['lx'].iloc[0] = l0
-
+#%%
 #preencher os valores em lx e dx
-while c < 116:
-    at['dx'] = at['lx'] * at['AT-2000-F']
+tam_2 = tam - 1
+while c < tam:
+    at['dx'] = at['lx'] * at[s]
     lx_2 =  at['lx'] - at['dx']
-    lx_2 = lx_2.iloc[0:115]
+    lx_2 = lx_2.iloc[0:tam_2]
     at['lx'].iloc[1:] = lx_2
     c +=1
 
 #%%
+
 #preencher os valores em v^x
 at['v^x'] = 1/((1+j)**at['IDADE'])
 
+#%%
 #preencher os valores em Dx e Nx
 at['Dx'] = at['lx'] * at['v^x']
 
 at['Nx'].iloc[0] = at['Dx'].sum()
 c = 1
-while c < 116:
-    at['Nx'].iloc[c] = at['Dx'].iloc[c:116].sum()
+while c < tam:
+    at['Nx'].iloc[c] = at['Dx'].iloc[c:tam].sum()
     c +=1
+    
 # %%
 #preencher os valores em Cx e Mx
 at['Cx'] = at['dx'] * at['v^x']
@@ -83,13 +109,7 @@ at['Cx'] = at['dx'] * at['v^x']
 
 at['Mx'].iloc[0] = at['Cx'].sum()
 c = 1
-while c < 116:
-    at['Mx'].iloc[c] = at['Cx'].iloc[c:116].sum()
+while c < tam:
+    at['Mx'].iloc[c] = at['Cx'].iloc[c:tam].sum()
     c +=1
     
-
-#%%
-brems15 = pd.DataFrame(df, columns = ['TÁBUA','BR-EMSmt-v.2015-f','BR-EMSmt-v.2015-m'])
-brems15.columns = ['IDADE', 'BR-EMSmt-v.2015-F', 'BR-EMSmt-v.2015-M']
-brems15.dropna(inplace=True)
-brems15 = brems15.iloc[1:]
