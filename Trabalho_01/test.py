e13508bf6f647bc30ce2333343b63a36f84052a0
Tomas <tomasbailez11@gmail.com>
2021-05-22 21:43:47 -0300

    corrigindo erros

--- Trabalho_01/test.py
+++ Trabalho_01/test.py
@@ -11,11 +11,11 @@ c = 0
 #montando o df de at-2000
 t = input('AT-2000 ou BR-EMSmt-v.2015? Selecione com A ou B\n')
 if t.upper() == 'A':
-    at = pd.DataFrame(df, columns = ['TÁBUA','AT-2000 FEMALE','AT-2000 MALE'])
-    at.columns = ['IDADE', 'AT-2000-F', 'AT-2000-M']
-    at.dropna(inplace=True)
-    at = at.iloc[1:]
-    tam = len(at['IDADE'])
+    dataset = pd.DataFrame(df, columns = ['TÁBUA','AT-2000 FEMALE','AT-2000 MALE'])
+    dataset.columns = ['IDADE', 'AT-2000-F', 'AT-2000-M']
+    dataset.dropna(inplace=True)
+    dataset = dataset.iloc[1:]
+    tam = len(dataset['IDADE'])
     while True:
         g = input("Selecione o gênero com F ou M?\n")
  
@@ -30,12 +30,12 @@ if t.upper() == 'A':
             print('Tente selecionar F ou M')
             
 elif t.upper() == 'B':  
-    at = pd.DataFrame(df, columns = ['TÁBUA','BR-EMSmt-v.2015-f','BR-EMSmt-v.2015-m'])
-    at.columns = ['IDADE', 'BR-EMSmt-v.2015-F', 'BR-EMSmt-v.2015-M']
-    at.dropna(inplace=True)
-    at = at.iloc[1:]
+    dataset = pd.DataFrame(df, columns = ['TÁBUA','BR-EMSmt-v.2015-f','BR-EMSmt-v.2015-m'])
+    dataset.columns = ['IDADE', 'BR-EMSmt-v.2015-F', 'BR-EMSmt-v.2015-M']
+    dataset.dropna(inplace=True)
+    dataset = dataset.iloc[1:]
     #at = at[:-1]
-    tam = len(at['IDADE'])
+    tam = len(dataset['IDADE'])
     while True:
         g = input("Selecione o gênero com F ou M?\n")
  
@@ -73,47 +73,47 @@ col_list = ['lx', 'dx','v^x','Dx','Nx','Cx','Mx']
 #%%
 #adiocionando todas as colunas ao dataframe
 for i in col_list:
-    at[i]= 0
+    dataset[i]= 0
     
 #atribuindo o valor de l0 para a primeira coluna de lx 
-at['lx'].iloc[0] = l0
+dataset['lx'].iloc[0] = l0
 #%%
 #preencher os valores em lx e dx
 tam_2 = tam - 1
 while c < tam:
-    at['dx'] = at['lx'] * at[s]
-    lx_2 =  at['lx'] - at['dx']
+    dataset['dx'] = dataset['lx'] * dataset[s]
+    lx_2 =  dataset['lx'] - dataset['dx']
     lx_2 = lx_2.iloc[0:tam_2]
-    at['lx'].iloc[1:] = lx_2
+    dataset['lx'].iloc[1:] = lx_2
     c +=1
 
 #%%
 
 #preencher os valores em v^x
-at['v^x'] = 1/((1+j)**at['IDADE'])
+dataset['v^x'] = 1/((1+j)**dataset['IDADE'])
 
 #%%
 #preencher os valores em Dx e Nx
-at['Dx'] = at['lx'] * at['v^x']
+dataset['Dx'] = dataset['lx'] * dataset['v^x']
 
-at['Nx'].iloc[0] = at['Dx'].sum()
+dataset['Nx'].iloc[0] = dataset['Dx'].sum()
 c = 1
 while c < tam:
-    at['Nx'].iloc[c] = at['Dx'].iloc[c:tam].sum()
+    dataset['Nx'].iloc[c] = dataset['Dx'].iloc[c:tam].sum()
     c +=1
     
 # %%
 #preencher os valores em Cx e Mx
 c = 0
 while c < tam_2:
-    at['Cx'].iloc[c] = at['dx'].iloc[c] * at['v^x'].iloc[c+1]
+    dataset['Cx'].iloc[c] = dataset['dx'].iloc[c] * dataset['v^x'].iloc[c+1]
     c += 1
     
 
 
-at['Mx'].iloc[0] = at['Cx'].sum()
+dataset['Mx'].iloc[0] = dataset['Cx'].sum()
 c = 1
 while c < tam:
-    at['Mx'].iloc[c] = at['Cx'].iloc[c:tam].sum()
+    dataset['Mx'].iloc[c] = dataset['Cx'].iloc[c:tam].sum()
     c +=1
     
